08dda1439e4914ea9a9cbc3151d5e1593e6c9df7
Jesenia Bernal <jese2209@gmail.com>
2023-06-16 12:28:59 -0700

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
-int main() {
-
-	 puts("\"Programming is like building a multilingual puzzle");
-	 return (0);
+/**
+  * main - C program Jesenia B
+  *
+  * Return: always 0.
+  **/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
