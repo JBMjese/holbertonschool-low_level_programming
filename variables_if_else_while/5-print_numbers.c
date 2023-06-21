d5ef4f2f8921208447f07209ee93403492aa1581
Jesenia Bernal <jese2209@gmail.com>
2023-06-21 16:35:33 -0700

    4

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - Print all the letters except q and e
+  * Return: Always 0.
+  **/
+int main(void)
+{
+	int numero = 0;
+
+	while (numero <= 9)
+	{
+		printf("%d ", numero);
+		numero++;
+	}
+	printf("\n");	
+	return (0);
+}
