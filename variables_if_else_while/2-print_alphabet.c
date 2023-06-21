ff658c2c75059cf52fddfbafb797be67f13d9449
Jesenia Bernal <jese2209@gmail.com>
2023-06-21 13:27:31 -0700

    2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  * main - The alphabet game
+  * Return: Always 0.
+  **/
+int main(void)
+{
+	char letra = 'a';
+	while (letra <= 'z')
+	{
+	putchar(letra);
+	letra++;
+	}
+	putchar('\n');
+	return (0);
+}
