89c3a93f679b7488243442548c8e6a166ac72837
Jesenia Bernal <jese2209@gmail.com>
2023-06-23 12:33:51 -0700

    3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_islower - lowercase character
+ *Return: 1 if character is lowercase, 0 otherwise.
+ **/
+int _islower(int a)
+
+{
+	if (a >= 'a' && a <= 'z')
+	{
+		return (1);
+	}
+		else
+	{
+		return (0);
+	}
+}
+
