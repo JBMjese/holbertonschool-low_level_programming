74e60c8946b77cf9c701462a7da05a1d9bd2243a
Jesenia Bernal <jese2209@gmail.com>
2023-06-23 09:18:54 -0700

    0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - numbers of base 10
+ *Return: Always 0.
+ **/
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+return (0);
+}
