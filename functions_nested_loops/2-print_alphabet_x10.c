01813a7f808e815fda07662f30a950d72c4a6da0
Jesenia Bernal <jese2209@gmail.com>
2023-06-23 12:08:11 -0700

    2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - alphabet 10 twice
+ *Return: Always 0.
+ **/
+void print_alphabet_x10(void)
+{
+	int v = 0;
+	char a;
+
+	while (v++ <= 9)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		_putchar(a);
+		_putchar('\n');
+	}
+}	
