c4eeb6cfe7a69e83953faf66665e0c57f481a508
Jesenia Bernal <jese2209@gmail.com>
2023-06-23 14:10:04 -0700

    7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,6 +8,10 @@ int print_last_digit(int n)
 
 {
 	int last_digit = n % 10;
-	printf("%d\n", last_digit)
-	return last_digit;
-}	
+	{
+		int last_digit = n % 10;
+		if (last_digit < 0)
+			last_digit *= -1;
+		_putchar(last_digit + '0');
+		return (last_digit);
+	}
