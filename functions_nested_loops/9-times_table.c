32c07fa2f4f017f75ea6e907e662c39ea879c705
Jesenia Bernal <jese2209@gmail.com>
2023-06-23 21:29:43 -0700

    9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -9,28 +9,27 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		 for (b = 0; b <= 9; b++)
-		 {
-			 c = a * b;
-			 if (b == 0)
-			 {
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+			if (b == 0)
+			{
 				_putchar(c + '0');
-				
-			 }
-			 else
-			 {
+			}
+			else if (c <= 9)
+			{
+				_putchar(32);
+				_putchar(c + '0');
+			}
+			else
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
+			if (b != 9)
+			{
 				_putchar(',');
-				_putchar(' ');
-				if (c <= 9)
-				{
-					_putchar(' ');
-					_putchar(c + '0');
-				}
-				else
-				{
-					_putchar((c / 10) + '0');
-					_putchar((c % 10) + '0');
-				}
+				_putchar(32);
 			}
 		}
 		_putchar('\n');
