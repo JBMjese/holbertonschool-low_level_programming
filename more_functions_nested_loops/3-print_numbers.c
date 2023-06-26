fee20d00d17b13a7bb59983d2d94d2d34947a1be
Jesenia Bernal <jese2209@gmail.com>
2023-06-25 17:57:41 -0700

    3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,13 @@
-#include "main.h"
+z#include "main.h"
 /**
  * print_numbers - Prints the numbers
  **/
 void print_numbers(void)
+	
 {
-
 	int i;
-	for (i = 0; i < 10; i++)
-		
+
+	for (i = 0; i < 10; i++)		
 	{
 		_putchar(i + '0');
 	}
