82f1358faa3b476227d18513383ef63f5ec38250
JBMjese <6935@holbertonstudents.com>
2023-06-28 19:46:03 -0700

    3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ ** _puts - prints a string
+ **@str: pointer to string
+  **Return: 0
+  **/
+void _puts(char *str)
+{
+	while (*str != '\0')
+
+	{
+		_putchar(*str):
+		str++;
+	}
+	putchar('\n');
+	return (0):
+}		
