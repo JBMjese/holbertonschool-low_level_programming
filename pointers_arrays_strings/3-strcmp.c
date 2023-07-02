b13d1eadec196fb4566c44625c14a78564803d9c
JBMjese <6935@holbertonstudents.com>
2023-07-02 15:03:11 -0700

    3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -7,7 +7,7 @@
   * Return: Integer less than, equal to, or greater than zero if s1 is
   * found, respectively, to be less than, to match, or be greater
   * than s2.
-  */ 
+  */
 int _strcmp(char *s1, char *s2)
 {
 	while (*s1 != '\0' && *s1 == *s2)
