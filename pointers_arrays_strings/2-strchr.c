ed87906f6a213e148a0424de59506dddf3d96cef
JBMjese <6935@holbertonstudents.com>
2023-07-02 18:21:44 -0700

    2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -5,7 +5,7 @@
   * @s: Pointer to the string to search.
   * @c: Character to locate.
   *
-  * Return: Pointer to the first occurrence of the character @c in the string @s,
+  * Return: Pointer to the first of the character @c in the string @s,
   *         or NULL if the character is not found.
   */
 char *_strchr(char *s, char c)
@@ -13,8 +13,8 @@ char *_strchr(char *s, char c)
 	while (*s != '\0')
 	{
 		if (*s == c)
-	return (s);
-	s++;
+		return (s);
+		s++;
 	}
 
 	if (*s == c)
