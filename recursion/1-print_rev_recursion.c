2a4fddb9e9f6e610825e9c4053ed17423a35c00b
JBMjese <6935@holbertonstudents.com>
2023-07-05 14:00:20 -0700

    1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  **print_rev_recursion - prints a string in reverse
+  **@s: pointer to string
+  *
+  **/
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		s = s + 1;
+	_print_rev_recursion(s);
+	_putchar(*(s - 1));
+	}
+}
