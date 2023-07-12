77da3b976974d8762adc9ac77d3e93f39322c753
JBMjese <6935@holbertonstudents.com>
2023-07-11 17:30:23 -0700

    0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,1 +1,12 @@
-
+#include <stdio.h>
+/**
+  ** main - prints its name
+ ** @argc: Argument counter
+ ** @argv: Argument vector
+ ** Return: Always 0
+ **/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+		printf("%s\n", argv[0]);
+			return (0);
+}
