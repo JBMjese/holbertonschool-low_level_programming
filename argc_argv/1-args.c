7851b0515f1593e1792fd22f1cd55c43e6817a18
JBMjese <6935@holbertonstudents.com>
2023-07-11 17:03:59 -0700

    1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -5,7 +5,7 @@
   ** @argv: Argument vector
   ** Return: Always 0
   **/
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
