163d2d8595c0043b236332cc9d1f7bce6fb5b8ac
JBMjese <6935@holbertonstudents.com>
2023-07-11 17:34:42 -0700

    3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-  ** main - multiplies two numbers.
-  ** @argc: Argument counter
-  ** @argv: Argument vector
-  ** Return: Always 0
+  * main - multiplies two numbers.
+  * @argc: Argument counter
+  * @argv: Argument vector
+  * Return: Always 0
   **/
 int main(int argc, char *argv[])
 {
 	int i;
-	int mul = 1;
+	int mul;
 
-	if (argc > 1)
+	mul = 1;
+
+	if (argc == 3)
 	{
 		for (i = 1; i < argc; i++)
 		{
