45fbc1d7685f91c226e9a61834f92ad4898c6d6e
JBMjese <6935@holbertonstudents.com>
2023-07-19 12:27:07 -0700

    2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -4,15 +4,14 @@
 /**
   * print_dog - prints a struct dog
   * @d:variable
-  * 
   */
 void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-	return;
+		return;
 	}
 	printf("Nombre: %s\n", (d->name != NULL) ? d->name : "(nil)");
 	printf("Edad: %.1f\n", d->age);
 	printf("Propietario: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
-}	
+}
