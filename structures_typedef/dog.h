c798caefaf6c85a50e3661aa9daeaa84e15b44ab
JBMjese <6935@holbertonstudents.com>
2023-07-19 09:15:04 -0700

    0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,18 +6,11 @@
    * @age: age
    * @owner: owner
    */
-typedef struct dog dog_t;
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 
-}; 
-
-int _putchar(char c);
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
+}dog_t;
 #endifi
