1f17ece0457d5673f8a1f7087af052d9a9e74b5a
JBMjese <6935@holbertonstudents.com>
2023-07-21 12:13:37 -0700

    2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -16,11 +16,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (array != NULL && cmp != NULL)
 	{	
 		for (int i = 0; i < size; i++)
-	{
+	}
 		if (cmp(array[i]) != 0)
 		{
 			return (i);
 		}
-	}
 	return (-1);
 }
