bef2be70ea84272b67a2dfbcecb069003a31549a
JBMjese <6935@holbertonstudents.com>
2023-07-22 18:43:40 -0700

    3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -2,14 +2,14 @@
 
 /**
   * main - Prints the result of simple operations.
-  * @argc: The number of arguments supplied to the program.
-  * @argv: An array of pointers to the arguments.
+  * @argc: The number of arguments.
+  * @argv: An array of pointers.
   *
   * Return: Always 0.
   */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int num1, num2;
+	int n1, n2;
 	char *op;
 
 	if (argc != 4)
@@ -18,20 +18,20 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		exit(98);
 	}
 
-	num1 = atoi(argv[1]);
+	n1 = atoi(argv[1]);
 	op = argv[2];
-	num2 = atoi(argv[3]);
+	n2 = atoi(argv[3]);
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*op == '/' && num2 == 0) ||
-	(*op == '%' && num2 == 0))
+	if ((*op == '/' && n2 == 0) ||
+	(*op == '%' && n2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	printf("%d\n", get_op_func(op)(num1, num2));
+	printf("%d\n", get_op_func(op)(n1, n2));
 	return (0);
 }
