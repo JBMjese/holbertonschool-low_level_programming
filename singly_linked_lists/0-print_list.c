e8846c7a17fcfa9e2d6f17dee1e13fd8e9613970
JBMjese <6935@holbertonstudents.com>
2023-08-03 13:14:18 -0700

    0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -5,22 +5,26 @@
   * Return: Number of nodes
   */
 size_t print_list(const list_t *h)
-{
+{	
 	size_t count;
 
 	count = 0;
 	
-	for (h != NULL; h = h->next)
-		count++;
+	while (h != NULL)
 	{	
 		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
+			h = h->next;
+			count += 1;
+			continue;
 		}
 	else
 	{
 		printf("[%u] %s\n", h->len, h->str);
-	}	
+		count += 1;
+		h = h->next;
+	}
 	}
 	return (count);
 }	
