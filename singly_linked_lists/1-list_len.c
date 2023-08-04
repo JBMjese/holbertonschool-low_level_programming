bd9f130a6e49888b54e3f95868bc480b7ac7f9bc
JBMjese <6935@holbertonstudents.com>
2023-08-04 09:30:57 -0700

    1

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,16 +1,18 @@
 #include "lists.h"
 /**
-  * list_len - function that returns the number of elements in a linked list_t list.
+  * list_len - function that returns the number of elements.
   * @h: pointer to list
   * Return: Number of elements 
   * */
 size_t list_len(const list_t *h)
 {
-	size_t count = 0
-	list_t *temp = h
+	size_t count = 0;
+	const list_t *temp = h;
 	
 	while (temp != NULL)
-	count++;
-	temp = temp->next;
+	{	
+		count++;
+		temp = temp->next;
+	}
 	return (count);
 }
