cbf908bd6e05e709f1f03498e3b70aa4d61d0fbd
JBMjese <6935@holbertonstudents.com>
2023-08-06 18:39:07 -0700

    5

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -12,6 +12,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *current = head;
 	
 	unsigned int i = 0;
+	
 	while (current != NULL && i < index)
 	{
 		current = current->next;
