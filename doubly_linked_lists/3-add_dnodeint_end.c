7fd42b770259d4f714fb10b1f7104ae24ba68705
JBMjese <6935@holbertonstudents.com>
2023-08-06 18:21:37 -0700

    3

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -20,6 +20,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	else
 	{
 		dlistint_t *current = *head;
+
 		while (current->next != NULL)
 		current = current->next;
 		current->next = new_node;
