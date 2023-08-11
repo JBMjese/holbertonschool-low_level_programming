f86adae54b62b6a20955e0e9d238044ad75782b6
JBMjese <6935@holbertonstudents.com>
2023-08-11 16:35:36 -0700

    1

---
+++ hash_tables/1-djb2.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+/**
+  * hash_djb2 - implementation of the djb2 algorithm
+  * @str: string used to generate hash value
+  * Return: hash value
+  */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash = 5381;
+	int c;
+
+	while ((c = *str++) != '\0') 
+	{
+		hash = ((hash << 5) + hash) + c;
+	}
+	return (hash);
+}
