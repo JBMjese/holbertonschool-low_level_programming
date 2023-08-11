ac2cdb495aeefd238c1e0c6a187166124bc18503
JBMjese <6935@holbertonstudents.com>
2023-08-11 16:30:16 -0700

    0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -7,25 +7,18 @@
   */
 hash_table_t *hash_table_create(unsigned long int size) 
 {
-	hash_table_t *ht = NULL;
+	hash_table_t *ht;
+	unsigned long int i;
+
 	ht = malloc(sizeof(hash_table_t));
-
 	if (ht == NULL)
-	{
-		return (NULL);
-	}
+	return (NULL);
+
 	ht->size = size;
-
-	ht->array = calloc(size, sizeof(hash_node_t *));
-
+	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
-	{
-		 free(ht);
-		 return (NULL);
-	}
-	for (unsigned long int i = 0; i < size; i++)
-	{
-		ht->array[i] = NULL;
-	}
+	return (NULL);
+	for (i = 0; i < size; i++)
+	ht->array[i] = NULL;
 	return (ht);
 }
