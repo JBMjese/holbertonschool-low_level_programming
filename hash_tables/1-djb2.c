41e495de2298ebb24451806933c95706e11c0238
JBMjese <6935@holbertonstudents.com>
2023-08-11 16:36:34 -0700

    1

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -9,7 +9,7 @@ unsigned long int hash_djb2(const unsigned char *str)
 	unsigned long int hash = 5381;
 	int c;
 
-	while ((c = *str++) != '\0') 
+	while ((c = *str++) != '\0')
 	{
 		hash = ((hash << 5) + hash) + c;
 	}
