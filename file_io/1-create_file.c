0ad12ae62118ec626463da46cc02168ff00a604c
JBMjese <6935@holbertonstudents.com>
2023-08-12 12:43:20 -0700

    1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,6 +14,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
 	if (fd == -1)
 	{
 		return (-1);
@@ -21,6 +22,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		int bytes_written = write(fd, text_content, strlen(text_content));
+
 		if (bytes_written == -1)
 		{
 			close(fd);
