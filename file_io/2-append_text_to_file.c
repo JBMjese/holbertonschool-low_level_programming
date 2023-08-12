d0645dc9151eb4accd054605bf2ba8515b4be5a4
JBMjese <6935@holbertonstudents.com>
2023-08-12 13:36:24 -0700

    2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -9,26 +9,21 @@
   */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	if (filename == NULL)
-	{
-		return (-1);
-	}
-	int fd = open(filename, O_WRONLY | O_APPEND, 0600);
+	int o, w, len = 0;
 
-	if (fd == -1)
-	{
-		return (-1);
-	}
+	if (filename == NULL)
+	return (-1);
+
 	if (text_content != NULL)
 	{
-		int bytes_written = write(fd, text_content, strlen(text_content));
-		if (bytes_written == -1)
-		{
-			close(fd);
-			return (-1);
-		}
-	}	
-	close(fd);
+		for (len = 0; text_content[len];)
+		len++;
+	}
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+	if (o == -1 || w == -1)
+	return (-1);
+
+	close(o);
 	return (1);
 }
-
