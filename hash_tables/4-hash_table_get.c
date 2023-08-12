2130bd808e5c3b45393b6b0ebd1a8147883f0825
JBMjese <6935@holbertonstudents.com>
2023-08-11 17:19:02 -0700

    4

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -9,14 +9,18 @@
   */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index = key_index(key, ht->size);
+	hash_node_t *node;
+	unsigned long int index;
 
-	hash_node_t *node = ht->table[index];
-	 if (node != NULL)
-	{
-		if (strcmp(node->key, key) == 0)
-		{
-			return (node->value);
-		}
-	}
-}	 
+	if (ht == NULL || key == NULL || *key == '\0')
+	return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+	return (NULL);
+	node = ht->array[index];
+	while (node && strcmp(node->key, key) != 0)
+	node = node->next;
+
+	return ((node == NULL) ? NULL : node->value);
+}
